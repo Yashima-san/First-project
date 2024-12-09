0471b6c744ee67149152f0c5be272433330e9f41
Yashima-san <liyashima18@gmail.com>
2024-12-09 22:30:55 +0700

    Delete Files directory

--- Files/Models/DatabaseAccess.cs
+++
@@ -1,8 +0,0 @@
-public class DatabaseAccess
-{
-    public List<Customer> GetCustomers()
-    {
-        // Получение списка клиентов из базы данных
-        // ...
-    }
-}
--- Files/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Files.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
